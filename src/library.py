e53d315877b96692da63c167c5c5decbbef3de25
PatrickMogul <patrick@easynode.pro>
2022-12-10 12:30:07 -0600

    upgrayyyed

--- src/library.py
+++ src/library.py
@@ -6,9 +6,21 @@ import time
 from os import environ
 from colorama import Fore, Back, Style
 from subprocess import PIPE, run
-from toolbox.library import print_stars, print_stars_reset, return_txt, load_var_file, disk_partitions, converted_unit, free_space_check, all_sys_info, set_main_or_test, ask_yes_no
+from toolbox.library import (
+    print_stars,
+    print_stars_reset,
+    return_txt,
+    load_var_file,
+    disk_partitions,
+    converted_unit,
+    free_space_check,
+    all_sys_info,
+    set_main_or_test,
+    ask_yes_no,
+)
 from toolbox.toolbox import menu_error, menu_ubuntu_updates, menu_reboot_server, finish_node
 from config import easy_env_fra
+
 
 def docker_check():
     status = subprocess.call(["docker"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -22,6 +34,7 @@ def docker_check():
         print_stars()
         raise SystemExit(0)
 
+
 def menu_findora() -> None:
     # menuTopperFull()
     for x in return_txt(easy_env_fra.findora_menu):
@@ -33,23 +46,27 @@ def menu_findora() -> None:
         if x:
             print(x)
 
+
 def refresh_stats() -> None:
     print_stars()
-    print(f'* Coming soon!')
+    print(f"* Coming soon!")
     print_stars()
     input("* Press ENTER to continue.")
+
 
 def check_balance_menu() -> None:
     print_stars()
-    print(f'* Coming soon!')
+    print(f"* Coming soon!")
     print_stars()
     input("* Press ENTER to continue.")
 
+
 def operating_system_updates() -> None:
     print_stars()
-    print(f'* Coming soon!')
+    print(f"* Coming soon!")
     print_stars()
     input("* Press ENTER to continue.")
+
 
 def server_disk_check() -> None:
     print_stars_reset()
@@ -60,15 +77,27 @@ def server_disk_check() -> None:
     total, used, free = shutil.disk_usage(easy_env_fra.our_disk_mount)
     total = str(converted_unit(total))
     used = str(converted_unit(used))
-    print("Disk: " + str(easy_env_fra.our_disk_mount) + "\n" + free_space_check(easy_env_fra.our_disk_mount) + " Free\n" + used + " Used\n" + total + " Total")
+    print(
+        "Disk: "
+        + str(easy_env_fra.our_disk_mount)
+        + "\n"
+        + free_space_check(easy_env_fra.our_disk_mount)
+        + " Free\n"
+        + used
+        + " Used\n"
+        + total
+        + " Total"
+    )
     print_stars()
     input("* Disk check complete, press ENTER to return to the main menu. ")
 
+
 def coming_soon() -> None:
     print_stars()
-    print(f'* Coming soon!')
+    print(f"* Coming soon!")
     print_stars()
     input("* Press ENTER to continue.")
+
 
 def menu_topper() -> None:
     Load1, Load5, Load15 = os.getloadavg()
@@ -77,51 +106,107 @@ def menu_topper() -> None:
     # get other validator data
     os.system("clear")
     print_stars()
-    print(f'{Style.RESET_ALL}* {Fore.Magenta}validator-toolbox for Findora FRA Validators by Easy Node   v{easy_env_fra.easy_version}{Style.RESET_ALL}   https://easynode.pro *')
+    print(
+        f"{Style.RESET_ALL}* {Fore.Magenta}validator-toolbox for Findora FRA Validators by Easy Node   v{easy_env_fra.easy_version}{Style.RESET_ALL}   https://easynode.pro *"
+    )
     print_stars()
-    print(f'* Server Hostname & IP:             {easy_env_fra.server_host_name}{Style.RESET_ALL} - {Fore.YELLOW}{easy_env_fra.our_external_ip}{Style.RESET_ALL}')
-    print(f'* Current disk space free: {Fore.CYAN}{free_space_check(easy_env_fra.our_disk_mount): >6}{Style.RESET_ALL}\n')
+    print(
+        f"* Server Hostname & IP:             {easy_env_fra.server_host_name}{Style.RESET_ALL} - {Fore.YELLOW}{easy_env_fra.our_external_ip}{Style.RESET_ALL}"
+    )
+    print(
+        f"* Current disk space free: {Fore.CYAN}{free_space_check(easy_env_fra.our_disk_mount): >6}{Style.RESET_ALL}\n"
+    )
     print_stars()
-    print(f"* CPU Load Averages: {round(Load1, 2)} over 1 min, {round(Load5, 2)} over 5 min, {round(Load15, 2)} over 15 min")
+    print(
+        f"* CPU Load Averages: {round(Load1, 2)} over 1 min, {round(Load5, 2)} over 5 min, {round(Load15, 2)} over 15 min"
+    )
     print_stars()
     return
-    
+
+
 def update_findora_container() -> None:
     print_stars()
-    print(f'* Running the update and restart may cause missed blocks, beware before proceeding!')
-    answer = ask_yes_no(f'* Are you sure you want to check for an upgrade and restart?')
+    print(f"* Running the update and restart may cause missed blocks, beware before proceeding!")
+    answer = ask_yes_no(f"* Are you sure you want to check for an upgrade and restart? (Y/N) ")
     if answer:
-        subprocess.call
-        run(f'cd ~/ && wget -O update_mainnet.sh https://raw.githubusercontent.com/easy-node-pro/findora-validator-scripts/main/easy_update_mainnet.sh')
+        subprocess.call(
+            [
+                "wget",
+                "-O",
+                f"/tmp/update_{environ.get('NETWORK')}.sh",
+                f"https://raw.githubusercontent.com/easy-node-pro/findora-validator-scripts/main/easy_update_{environ.get('NETWORK')}.sh",
+            ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        os.system("clear")
+        print(
+            f"* We will show the output of the upgrade & restart now, this may miss a block(s) depending on your timing."
+        )
+        subprocess.call(["bash", "-x", f"/tmp/update_{environ.get('NETWORK')}.sh"])
+        print_stars()
+        input(
+            f"* Setup has completed. Once you are synced up (catching_up=False) you are ready to create your validator on-chain or migrate from another server onto this server.\n* Press any key to continue."
+        )
     return
+
 
 def update_fn_wallet() -> None:
     print_stars()
-    print(f'* Coming Soon!')
+    print(f"* This option upgrades the fn wallet application.")
+    answer = ask_yes_no(f"* Do you want to upgrade fn now? (Y/N) ")
+    if answer:
+        subprocess.call(
+            [
+                "wget",
+                "-O",
+                f"/tmp/fn_update_{environ.get('NETWORK')}.sh",
+                f"https://raw.githubusercontent.com/easy-node-pro/findora-validator-scripts/main/fn_update_{environ.get('NETWORK')}.sh",
+            ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        os.system("clear")
+        print(f"* We will show the output of the upgrade now.")
+        subprocess.call(["bash", "-x", f"/tmp/fn_update_{environ.get('NETWORK')}.sh"])
     return
+
 
 def findora_installer() -> None:
     # Run installer ya'll!
-    print(f'* Welcome to EasyNode.PRO Validator Toolbox for Findora!')
-    print(f"* We've detected that Docker is properly installed for this user, excellent!")
-    print(f"* It doesn't look like you have Findora installed.")
-    print(f"* We will setup Findora validator on this server with a brand new wallet and start syncing with the blockchain.")
-    answer = ask_yes_no(f'* Do you want to install it now? (Y/N) ')
+    print(
+        f"* Welcome to EasyNode.PRO Validator Toolbox for Findora!\n* We've detected that Docker is properly installed for this user, excellent!\n* It doesn't look like you have Findora installed.\n* We will setup Findora validator on this server with a brand new wallet and start syncing with the blockchain."
+    )
+    answer = ask_yes_no(f"* Do you want to install it now? (Y/N) ")
     if answer:
         # mainnet or testnet
         set_main_or_test()
-        subprocess.call(["wget", f"https://raw.githubusercontent.com/easy-node-pro/findora-validator-scripts/main/easy_install_{environ.get('NETWORK')}.sh", "-O", f"/tmp/install_{environ.get('NETWORK')}.sh"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.call(
+            [
+                "wget",
+                f"https://raw.githubusercontent.com/easy-node-pro/findora-validator-scripts/main/easy_install_{environ.get('NETWORK')}.sh",
+                "-O",
+                f"/tmp/install_{environ.get('NETWORK')}.sh",
+            ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
         os.system("clear")
         print_stars()
-        print(f'* We will show the output of the installation, this will take some time to download and unpack.')
-        print(f'* Starting Findora installation now.')
+        print(
+            f"* We will show the output of the installation, this will take some time to download and unpack.\n* Starting Findora installation now."
+        )
         print_stars()
         time.sleep(3)
         print_stars()
         subprocess.call(["bash", "-x", f"/tmp/install_{environ.get('NETWORK')}.sh"])
         print_stars()
-        input(f'* Setup has completed. Once you are synced up (catching_up=False) you are ready to create your validator on-chain or migrate from another server onto this server.\n* Press any key to continue.')
-    print_stars()
+        input(
+            f"* Setup has completed. Once you are synced up (catching_up=False) you are ready to create your validator on-chain or migrate from another server onto this server.\n* Press any key to continue."
+        )
+    else:
+        raise SystemExit(0)
+
 
 def run_findora_menu() -> None:
     menu_options = {
