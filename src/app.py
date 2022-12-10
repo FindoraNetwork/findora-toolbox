d1bbf8c62a13afe085d672c55070ec1ac53263de
PatrickMogul <patrick@easynode.pro>
2022-12-10 13:02:44 -0600

    run menu after installer

--- src/app.py
+++ src/app.py
@@ -19,22 +19,21 @@ if __name__ == "__main__":
     if os.path.exists('/usr/local/bin/fn') is False:
         # Nope, let's ask to install!
         findora_installer()
-    else:
-        # fn is found, is the container running?
-        # Set the container name
-        container_name = "findorad"
+    # fn is found, is the container running?
+    # Set the container name
+    container_name = "findorad"
 
-        # Run the 'docker ps' command and filter the output using 'grep'
-        status = subprocess.call(["docker", "ps", "--filter", f"name={container_name}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # Run the 'docker ps' command and filter the output using 'grep'
+    status = subprocess.call(["docker", "ps", "--filter", f"name={container_name}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-        # Check the status and print a message
-        if status == 0:
-            # Launch menu, we're good to go!
-            print(f"* The container '{container_name}' is running.")
-            print_stars()
-            run_findora_menu()
-
-        else:
-            # Container is not running, ruh roh!
-            print(f"* The container '{container_name}' is not running.")
+    # Check the status and print a message
+    if status == 0:
+        # Launch menu, we're good to go!
+        print(f"* The container '{container_name}' is running.")
         print_stars()
+        run_findora_menu()
+
+    else:
+        # Container is not running, ruh roh!
+        print(f"* The container '{container_name}' is not running.")
+    print_stars()
