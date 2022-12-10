1355800dc29b654ff8fe8d849ebc9815d862f8ad
PatrickMogul <patrick@easynode.pro>
2022-12-10 11:51:39 -0600

    get out!!

--- src/library.py
+++ src/library.py
@@ -106,7 +106,7 @@ def findora_installer() -> None:
         # mainnet or testnet
         set_main_or_test()
         subprocess.run(["wget", f"https://raw.githubusercontent.com/easy-node-pro/findora-validator-scripts/main/easy_install_{environ.get('NETWORK')}.sh", "-O", f"/tmp/install_{environ.get('NETWORK')}.sh"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        subprocess.run(["bash", "-x", f"/tmp/install_{environ.get('NETWORK')}.sh"], stdout=PIPE, stderr=PIPE)
+        subprocess.getoutput(["bash", "-x", f"/tmp/install_{environ.get('NETWORK')}.sh"])
         print_stars()
         print(f'* Setup has completed. Once you are synced up (catching_up=False) you are ready to create your validator on-chain or migrate from another server onto this server.')
         raise SystemExit(0)
