5e24fd995cc012cd1304fffb10021f4fa1ef521c
PatrickMogul <patrick@easynode.pro>
2022-12-10 14:24:16 -0600

    test

--- src/library.py
+++ src/library.py
@@ -45,7 +45,6 @@ def refresh_stats() -> None:
     print_stars()
     try:
         output = subprocess.check_output(["curl", "http://localhost:26657/status"], stderr=subprocess.STDOUT, universal_newlines=True)
-        print(output)
     except:
         print("* Is your container running?")
     print_stars()
