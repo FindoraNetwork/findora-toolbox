fe006f8b6bef94d194422321dddde5f13b6bfa5c
PatrickMogul <patrick@easynode.pro>
2022-12-09 20:48:13 -0600

    moar

--- src/library.py
+++ src/library.py
@@ -79,6 +79,7 @@ def menu_topper() -> None:
     print(f'* Current disk space free: {Fore.CYAN}{freeSpaceCheck(validatorToolbox.our_disk_mount): >6}{Style.RESET_ALL}\n')
     printStars()
     print(f"* CPU Load Averages: {round(Load1, 2)} over 1 min, {round(Load5, 2)} over 5 min, {round(Load15, 2)} over 15 min")
+    printStars()
     return
     
 
