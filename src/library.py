bace65a5878617c43395fa4fa41e818c2aaf9ec1
PatrickMogul <patrick@easynode.pro>
2022-12-10 14:36:03 -0600

    better

--- src/library.py
+++ src/library.py
@@ -61,7 +61,7 @@ def refresh_fn_stats() -> None:
         output = subprocess.check_output(["fn", "show"])
         os.system("clear")
         print_stars()
-        print(output)
+        print(f'{output}')
     except:
         print("* Error!")
     print_stars()
