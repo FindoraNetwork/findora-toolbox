8b0f75678b3e65f9f5b074d3ec391821900eae3a
PatrickMogul <patrick@easynode.pro>
2022-12-10 20:33:24 -0600

    try

--- src/library.py
+++ src/library.py
@@ -66,7 +66,7 @@ def refresh_wallet_stats() -> None:
     try:
         output = subprocess.check_output(["curl", "http://localhost:26657/status"])
         output = output.decode().split("\n")
-        data = json.loads(output)
+        data = json.dumps(output)
         # status_code = int(output[1])
         print_stars()
         pprint(data)
