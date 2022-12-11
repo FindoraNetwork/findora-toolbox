9e9e5d8648a4d55e375c046930a9a49622bf4901
PatrickMogul <patrick@easynode.pro>
2022-12-11 00:43:57 -0600

    chown?

--- src/library.py
+++ src/library.py
@@ -313,6 +313,7 @@ def migrate_to_server() -> None:
                 print_stars()
                 # start installing
                 print('* Copying Files...')
+                subprocess.run(["sudo", "chown", "-R", f"{easy_env_fra.active_user_name}:{easy_env_fra.active_user_name}", easy_env_fra.findora_root])
                 shutil.copy(f'{easy_env_fra.migrate_dir}/tmp.gen.keypair', f'{easy_env_fra.findora_root}/{environ.get("FRA_NETWORK")}/{environ.get("FRA_NETWORK")}_node.key')
                 if os.path.exists(f'{easy_env_fra.migrate_dir}/priv_validator_key.json'): 
                     shutil.copy(f'{easy_env_fra.migrate_dir}/priv_validator_key.json', f'{easy_env_fra.findora_root}/{environ.get("FRA_NETWORK")}/tendermint/priv_validator_key.json')
