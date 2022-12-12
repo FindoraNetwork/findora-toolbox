93740c9504047678a0932ba8b5a6d7f40d37485a
PatrickMogul <patrick@easynode.pro>
2022-12-11 21:27:35 -0600

    Files checked, then displayed

--- src/library.py
+++ src/library.py
@@ -372,7 +372,7 @@ def run_container_update(status=False) -> None:
     update_findora_container(status)
 
 def migration_menu_option() -> None:
-    if os.path.exists(f'{easy_env_fra.migrate_dir}'):
+    if os.path.exists(f'{easy_env_fra.migrate_dir}/tmp.gen.keypair') and os.path.exists(f'{easy_env_fra.migrate_dir}/priv_validator_key.json') or os.path.exists(f"{easy_env_fra.migrate_dir}/priv_validator_key.json"):
         print(f'* 888 -  {Fore.CYAN}Migrate To This Server    {Fore.MAGENTA}- Migrate from another server to this server.')
 
 def run_findora_menu() -> None:
