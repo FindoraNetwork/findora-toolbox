393bf5ff79210a2392434316f9e8b77841c55490
PatrickMogul <patrick@easynode.pro>
2022-12-15 19:12:55 -0600

    good migrate, no junk left behind.

--- src/config.py
+++ src/config.py
@@ -11,7 +11,7 @@ def getUrl() -> None:
 
 
 class easy_env_fra:
-    easy_version = "1.1.4"
+    easy_version = "1.1.5"
     server_host_name = socket.gethostname()
     user_home_dir = os.path.expanduser("~")
     dotenv_file = f"{user_home_dir}/.easynode.env"
