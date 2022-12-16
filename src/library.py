3d948900465d6633be85d1b6f66d9733939f8d83
PatrickMogul <patrick@easynode.pro>
2022-12-15 21:16:00 -0600

    fix function name, we upgraded

--- src/library.py
+++ src/library.py
@@ -50,7 +50,7 @@ string_stars_reset = print_stuff(reset=1).stringStars
 
 
 # loader intro splash screen
-def loaderIntro():
+def loader_intro():
     p = f"""
     {string_stars()}
                     ____ ____ ____ ____ _________ ____ ____ ____ ____           
