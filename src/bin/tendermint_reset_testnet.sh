ae4e452b546645ec2742a3d8ab111cc6e56195e4
patrickmogul <patrick@easynode.pro>
2023-01-19 18:30:00 -0600

    Scripts

---
+++ src/bin/tendermint_reset_testnet.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+USERNAME=$USER
+ENV=prod
+NAMESPACE=testnet
+SERV_URL=https://${ENV}-${NAMESPACE}.${ENV}.findora.org
+LIVE_VERSION=$(curl -s https://${ENV}-${NAMESPACE}.${ENV}.findora.org:8668/version | awk -F\  '{print $2}')
+FINDORAD_IMG=findoranetwork/findorad:${LIVE_VERSION}
+export ROOT_DIR=/data/findora/${NAMESPACE}
+keypath=${ROOT_DIR}/${NAMESPACE}_node.key
+FN=${ROOT_DIR}/bin/fn
+CONTAINER_NAME=findorad
+
+# if you're re-running this for some reason, stop and remove findorad
+if docker ps -a --format '{{.Names}}' | grep -Eq ${CONTAINER_NAME}; then
+  echo -e "Findorad Container found, stopping container to restart."
+  docker stop findorad
+  docker rm findorad
+  rm -rf /data/findora/mainnet/tendermint/config/addrbook.json
+else
+  echo 'Findorad container stopped or does not exist, continuing.'
+fi
+
+docker run --rm -v ${ROOT_DIR}/tendermint:/root/.tendermint ${FINDORAD_IMG} init --${NAMESPACE} || exit 1
+
+echo -e "* Tendermint has been reconfigured, run the update_version script or option to get back online."
