02afbef41bb86a1287187ab17d2c1d02c9bded73
Patrick Mogul <patrick@easynode.pro>
2023-09-23 16:56:20 -0500

    zero out

--- src/shared.py
+++ src/shared.py
@@ -43,6 +43,8 @@ def get_live_version(server_url):
 
 
 def format_duration(seconds):
+    if seconds <= 0:
+        seconds = 0
     """Convert duration in seconds to hms format."""
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
