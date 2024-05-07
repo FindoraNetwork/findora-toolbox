3b483f967b3afb54bc84266dddfeb8a1fdbe392e
Patrick Mogul <patrick@easynode.pro>
2024-05-07 16:08:22 -0500

    fix scrpt

--- src/test_fn_stats.py
+++ src/test_fn_stats.py
@@ -3,14 +3,18 @@ import sys
 import json
 
 print('* Getting fn stats... ', end='', flush=True)
-output = get_fn_values()
+public_address, balance, server_url, delegation_info, validator_address_evm = (
+    get_fn_values()
+)
 print('Completed', flush=True)
 
 print('* Compiling fn information... ', end='', flush=True)
-findora_validator_stats, validator_address, public_address = process_fn_stats(output)
-findora_validator_stats_str = json.dumps(findora_validator_stats, indent=4)
+findora_validator_stats = process_fn_stats(
+    validator_address_evm, balance, server_url, delegation_info
+)
+findora_validator_stats = json.dumps(findora_validator_stats, indent=4)
 print('Completed', flush=True)
 
-print('* findora_validator_stats:', findora_validator_stats_str)
-print('* validator_address:', validator_address)
+print('* findora_validator_stats:', findora_validator_stats)
+print('* validator_address:', validator_address_evm)
 print('* public_address:', public_address)
