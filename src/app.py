a566f7330fdd6099fa1e4dc4118f980ac8c2c744
Patrick Mogul <patrick@easynode.pro>
2024-05-11 10:05:58 -0500

    chore: Update Findora Toolbox to Fractal Toolbox in app.py

--- src/app.py
+++ src/app.py
@@ -23,10 +23,9 @@ def main() -> None:
     
     # Check for previous version of the toolbox, halt if found until upgraded manually
     if os.path.exists(f"{config.user_home_dir}/.findora.env"):
-        print_stars()
-        print("You have a previous version of the Findora Toolbox installed. Please remove it before continuing.")
-        print("Run the following command to remove the previous version:")
-        print("wget https://raw.githubusercontent.com/FindoraNetwork/findora-toolbox/main/src/bin/fractal.sh && chmod +x fractal.sh && rm ~/findora.sh && mv ~/findora-toolbox ~/fractal-toolbox && mv .findora.env .fractal.env && ./fractal.sh -u")
+        print("You have the Findora Toolbox installed and not the new Fractal Toolbox.")
+        print("Please run the following command to convert from the findora toolbox to fractal toolbox and start the upgrade process:")
+        print("cd && wget -O fractal.sh https://raw.githubusercontent.com/FindoraNetwork/findora-toolbox/main/src/bin/fractal.sh && chmod +x fractal.sh && rm ~/findora.sh && mv ~/findora-toolbox ~/fractal-toolbox && mv .findora.env .fractal.env && ./fractal.sh -u")
         print_stars()
         finish_node()
 
